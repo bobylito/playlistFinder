66924e724c468b573b1493ff0555d42974c6e6d2
bobylito <bobylito@gmail.com>
2018-08-14 09:31:29 +0200

    refactor(scrapper): reorganize functions order
    And add comments

--- scrapper/index.js
+++ scrapper/index.js
@@ -6,7 +6,6 @@ var tailƒ = require('lodash/tail');
 if(!process.env.SPOTIFY_ID) throw new Error('SPOTIFY_ID must be defined');
 if(!process.env.SPOTIFY_SECRET) throw new Error('SPOTIFY_SECRET must be defined');
 
-// credentials are optional
 var spotifyApi = new SpotifyWebApi({
   clientId : process.env.SPOTIFY_ID,
   clientSecret : process.env.SPOTIFY_SECRET,
@@ -15,7 +14,7 @@ var spotifyApi = new SpotifyWebApi({
 // create playlists folder
 try {
   fs.mkdirSync('playlists');
-} catch(e) { /* ignore */ }
+} catch(e) { /* ignore if it exists */ }
 
 var commandLineUser = process.argv[2];
 
@@ -27,7 +26,10 @@ if(commandLineUser) {
   rawusers = require('../data/users.json');
 }
 
+// stats
 var totalPlaylists = 0;
+
+// 0 - going through the list of curator one by one
 
 (function fetchNextUser(users) {
   if(users.length === 0) {
@@ -39,6 +41,8 @@ var totalPlaylists = 0;
     fetchNextUser(tailƒ(users));
   });
 })(rawusers)
+
+// 1 - fetch the user's playlist. Makes sure that we have the correct access token.
 
 function scrapUserPlaylists(user) {
   return spotifyApi.clientCredentialsGrant()
@@ -57,6 +61,112 @@ function scrapUserPlaylists(user) {
       console.error(e);
     });
 }
+
+// 2 - Actually fetches the user's playlist
+
+function getAllPlaylists(userID, previousPlaylists, end) {
+  if(end) return Promise.resolve(processPlaylists(previousPlaylists));
+
+  var playlists = previousPlaylists || [];
+  return spotifyApi.getUserPlaylists(userID, {
+    limit: 50,
+    offset: playlists.length
+  }).then(function(response) {
+    var body = response.body;
+    if(body.next) return getAllPlaylists(userID, playlists.concat(body.items));
+    else return getAllPlaylists(userID, playlists.concat(body.items), true);
+  }, (e) => handleSpotifyAPIError(
+      () => getAllPlaylists(userID, previousPlaylists),
+      () => getAllPlaylists(userID, previousPlaylists, true), // why would it fail??
+      e,
+      `user playlists from ${userID}`,
+    )
+  );
+}
+
+// 2bis - process the list of playlists to their serialization format
+
+function processPlaylists(pls) {
+  return pls.map(function(pl) {
+    return {
+      id: pl.id,
+      owner: pl.owner.id,
+      ownerHref: pl.owner['external_urls'].spotify,
+      name: pl.name,
+      description: '',
+      images: pl.images,
+      followers: 0,
+      songs: [],
+      artists: [],
+      href: pl['external_urls'].spotify
+    };
+  });
+}
+
+// 3 - Iterates over the user's playlists to add the songs and artists
+
+function getSongsAndArtistsForPlaylists(playlists, processedPlaylists) {
+  var newProcessedPlaylists = processedPlaylists || [];
+  if(playlists.length === 0) return newProcessedPlaylists;
+
+  var head = headƒ(playlists);
+  return getSongsAndArtists(head)
+    .then(function(songsAndArtists) {
+      head.songs = songsAndArtists.songs;
+      head.artists = songsAndArtists.artists;
+      newProcessedPlaylists.push(head);
+      return getSongsAndArtistsForPlaylists(tailƒ(playlists), newProcessedPlaylists);
+    }, (e) => handleSpotifyAPIError(
+      () => getSongsAndArtistsForPlaylists(playlists, processedPlaylists),
+      () => getSongsAndArtistsForPlaylists(tailƒ(playlists), processedPlaylists),
+      e,
+      `songs and artists for ${head.name} from ${head.owner}`,
+    )
+  );
+}
+
+// 4 - Get songs and artists for a single playlist
+
+function getSongsAndArtists(pl, previousTracks, end) {
+  if(end) return Promise.resolve(processSongsAndArtists(previousTracks));
+
+  var tracks = previousTracks || [];
+  return spotifyApi.getPlaylistTracks(pl.owner, pl.id, {fields: 'items(track(name,album(name),artists(name)))'})
+    .then(function(response) {
+      var body = response.body;
+      var items = tracks.concat(body.items);
+      if(body.next) return getSongsAndArtists(pl, items);
+      else return getSongsAndArtists(pl, items, true);
+    }, (e) => handleSpotifyAPIError(
+      () => getSongsAndArtists(pl, previousTracks),
+      () => getSongsAndArtists(pl, previousTracks, true),
+      e,
+      `songs and artists (browsing tracks) for ${pl.name} from ${pl.owner}`,
+    )
+  );
+}
+
+// 5 - Transforms songs into a list of unique artists and songs
+
+function processSongsAndArtists(tracks) {
+  var artists = {};
+  var songs = tracks.reduce(function(memo, container) {
+    var track = container.track;
+    if(track !== null) {
+      track.artists.forEach(function(artist) {
+        artists[artist.name] = artist.name;
+      });
+      memo.push(track.name);
+    }
+    return memo;
+  }, []);
+  return {
+    songs: songs,
+    artists: Object.keys(artists),
+  };
+}
+
+// 6 - Adds the description and number of followers to the playlists
 
 function getPlaylistFollowers(playlists, processedPlaylists) {
   var newProcessedPlaylists = processedPlaylists || [];
@@ -79,100 +189,13 @@ function getPlaylistFollowers(playlists, processedPlaylists) {
   );
 }
 
-function getSongsAndArtistsForPlaylists(playlists, processedPlaylists) {
-  var newProcessedPlaylists = processedPlaylists || [];
-  if(playlists.length === 0) return newProcessedPlaylists;
-
-  var head = headƒ(playlists);
-  return getSongsAndArtists(head)
-    .then(function(songsAndArtists) {
-      head.songs = songsAndArtists.songs;
-      head.artists = songsAndArtists.artists;
-      newProcessedPlaylists.push(head);
-      return getSongsAndArtistsForPlaylists(tailƒ(playlists), newProcessedPlaylists);
-    }, (e) => handleSpotifyAPIError(
-      () => getSongsAndArtistsForPlaylists(playlists, processedPlaylists),
-      () => getSongsAndArtistsForPlaylists(tailƒ(playlists), processedPlaylists),
-      e,
-      `songs and artists for ${head.name} from ${head.owner}`,
-    )
-  );
-}
-
-function getSongsAndArtists(pl, previousTracks, end) {
-  if(end) return Promise.resolve(processSongsAndArtists(previousTracks));
-
-  var tracks = previousTracks || [];
-  return spotifyApi.getPlaylistTracks(pl.owner, pl.id, {fields: 'items(track(name,album(name),artists(name)))'})
-    .then(function(response) {
-      var body = response.body;
-      var items = tracks.concat(body.items);
-      if(body.next) return getSongsAndArtists(pl, items);
-      else return getSongsAndArtists(pl, items, true);
-    }, (e) => handleSpotifyAPIError(
-      () => getSongsAndArtists(pl, previousTracks),
-      () => getSongsAndArtists(pl, previousTracks, true),
-      e,
-      `songs and artists (browsing tracks) for ${pl.name} from ${pl.owner}`,
-    )
-  );
-}
-
-function processSongsAndArtists(tracks) {
-  var artists = {};
-  var songs = tracks.reduce(function(memo, container) {
-    var track = container.track;
-    if(track !== null) {
-      track.artists.forEach(function(artist) {
-        artists[artist.name] = artist.name;
-      });
-      memo.push(track.name);
-    }
-    return memo;
-  }, []);
-  return {
-    songs: songs,
-    artists: Object.keys(artists),
-  };
-}
-
-function getAllPlaylists(userID, previousPlaylists, end) {
-  if(end) return Promise.resolve(processPlaylists(previousPlaylists));
-
-  var playlists = previousPlaylists || [];
-  return spotifyApi.getUserPlaylists(userID, {
-    limit: 50,
-    offset: playlists.length
-  }).then(function(response) {
-    var body = response.body;
-    if(body.next) return getAllPlaylists(userID, playlists.concat(body.items));
-    else return getAllPlaylists(userID, playlists.concat(body.items), true);
-  }, (e) => handleSpotifyAPIError(
-      () => getAllPlaylists(userID, previousPlaylists),
-      () => getAllPlaylists(userID, previousPlaylists, true), // why would it fail??
-      e,
-      `user playlists from ${userID}`,
-    )
-  );
-}
-
-function processPlaylists(pls) {
-  return pls.map(function(pl) {
-    return {
-      id: pl.id,
-      owner: pl.owner.id,
-      ownerHref: pl.owner['external_urls'].spotify,
-      name: pl.name,
-      description: '',
-      images: pl.images,
-      followers: 0,
-      songs: [],
-      artists: [],
-      href: pl['external_urls'].spotify
-    };
-  });
-}
-
+/**
+ * Generic error handling for spotify HTTP errors
+ * @param {function} functionRetry function to call when the error can be recovered
+ * @param {function} functionSkip function to call when the error can NOT be recovered
+ * @param {exception} e exception triggered by the spotify client
+ * @param {*} callCtx string containing context about what happened. Useful to understand the reason or where it happened
+ */
 function handleSpotifyAPIError(functionRetry, functionSkip, e, callCtx) {
   if(e.statusCode === 429) {
     console.log(`Error fetching data - rate limit - ${callCtx} - retrying in 10s`);
