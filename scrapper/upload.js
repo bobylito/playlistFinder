4ba1d02531b1523adff5c589315bbc9c6e82a413
bobylito <bobylito@gmail.com>
2019-11-23 16:31:39 +0100

    feat(upload): clean index (using move) + prettier

--- scrapper/upload.js
+++ scrapper/upload.js
@@ -1,19 +1,19 @@
-const algolia = require("algoliasearch");
-const process = require("process");
-const { getAllPlaylists, connectToDB } = require("./lib/playlists");
-const ora = require("ora");
+const algolia = require('algoliasearch');
+const process = require('process');
+const {getAllPlaylists, connectToDB} = require('./lib/playlists');
+const ora = require('ora');
 
 if (!process.env.ALGOLIA_APPLICATION_ID)
-  throw new Error("ALGOLIA_APPLICATION_ID must be defined");
+  throw new Error('ALGOLIA_APPLICATION_ID must be defined');
 if (!process.env.ALGOLIA_ADMIN_KEY)
-  throw new Error("ALGOLIA_ADMIN_KEY must be defined");
+  throw new Error('ALGOLIA_ADMIN_KEY must be defined');
 
 const appID = process.env.ALGOLIA_APPLICATION_ID;
 const apiKey = process.env.ALGOLIA_ADMIN_KEY;
 
-const client = algolia(appID, apiKey, { timeout: 60000 });
+const client = algolia(appID, apiKey, {timeout: 60000});
 
-const indexName = "spotify-search";
+const indexName = 'spotify-search';
 
 function addObjectId(objects) {
   return objects.map(function(o) {
@@ -23,14 +23,30 @@ function addObjectId(objects) {
 }
 
 connectToDB().then(async db => {
-  const index = client.initIndex(indexName);
+  const spinnerSetup = ora(`Setting up new index`).start();
+  const oldIndex = client.initIndex(indexName);
+  const oldSettings = await oldIndex.getSettings();
+  const tempIndexName = `${indexName}_temp`;
+  const index = client.initIndex(tempIndexName);
+  await index.setSettings(oldSettings);
+  spinnerSetup.succeed();
+
+  const spinnerRead = ora(`Reading DB for playlists`).start();
   const genPlaylists = getAllPlaylists(db);
+  spinnerRead.succeed();
+
   for await (let pls of genPlaylists) {
     const spinner = ora(`Uploading ${pls.length} playlists`).start();
     const plsWithObjectID = addObjectId(pls);
-    const { taskID } = await index.addObjects(plsWithObjectID);
+    const {taskID} = await index.addObjects(plsWithObjectID);
     await index.waitTask(taskID);
     spinner.succeed();
   }
+
+  const spinnerMove = ora('Moving temp index').start();
+  const {taskID} = await client.moveIndex(tempIndexName, indexName);
+  await index.waitTask(taskID);
+  spinnerMove.succeed();
+
   process.exit(0);
 });
